12b399b8f9360bba2aa2b0a0e145c86ba70e7a2b
Gamingstein <timeblackhole980@gmail.com>
2025-01-06 15:14:22 +0530

    test2 multer fix

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (_req, _file, cb) {
-    cb(null, "/tmp/uploads");
+    cb(null, "$HOME/uploads");
     // cb(null, "./public/temp");
   },
   filename: function (_req, file, cb) {
